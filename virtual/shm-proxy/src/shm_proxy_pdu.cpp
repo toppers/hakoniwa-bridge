1ffc6b15fa33fe2e496c02d41f341b2834d34786
Takashi Mori <kanetugu2015@gmail.com>
2024-04-11 09:37:46 +0900

    add timing for write

--- virtual/shm-proxy/src/shm_proxy_pdu.cpp
+++ virtual/shm-proxy/src/shm_proxy_pdu.cpp
@@ -26,7 +26,11 @@ bool shm_proxy_pdu_data_initialize(ShmProxyPduType& shm_proxy_pdu)
 {
     for (const hako::asset::Robot& robot : shm_proxy_pdu.robots) {
         for (const hako::asset::PduWriter& writer : robot.pdu_writers) {
-            PduWriter w(writer.channel_id, writer.pdu_size);
+            if (writer.write_cycle == 0) {
+                std::cerr << "ERROR: write_cycle is invalid(0). topic_name: " << writer.name << std::endl;
+                return false;
+            }
+            PduWriter w(writer.channel_id, writer.pdu_size, writer.write_cycle);
 
             z_owned_closure_sample_t callback = z_closure(shm_proxy_pdu_data_handler, NULL);
             w.sub = z_declare_subscriber(z_loan(shm_proxy_pdu.session.s), z_keyexpr(writer.name.c_str()), z_move(callback), NULL);
@@ -38,16 +42,20 @@ bool shm_proxy_pdu_data_initialize(ShmProxyPduType& shm_proxy_pdu)
             std::cout << "INFO: subscriber topic name = " << writer.name << std::endl;
         }
         for (const hako::asset::PduReader& reader : robot.pdu_readers) {
-            PduReader r(reader.channel_id, reader.pdu_size);
+            if (reader.write_cycle == 0) {
+                std::cerr << "ERROR: write_cycle is invalid(0). topic_name: " << reader.name << std::endl;
+                return false;
+            }
+            PduReader r(reader.channel_id, reader.pdu_size, reader.write_cycle);
 
             r.pub = z_declare_publisher(z_loan(shm_proxy_pdu.session.s), z_keyexpr(reader.name.c_str()), NULL);
             if (!z_check(r.pub)) {
-                std::cout << "Unable to declare Publisher for key expression!" << std::endl;
+                std::cerr << "Unable to declare Publisher for key expression!" << std::endl;
                 return false;
             }
             auto result = shm_proxy_pdu.pdu_reader_map.emplace(reader.name, std::move(r));
             if (!result.second) {
-                std::cout << "Failed to insert PduWriter into map." << std::endl;
+                std::cerr << "Failed to insert PduWriter into map." << std::endl;
                 return false;
             }
             std::cout << "INFO: publisher topic name = " << reader.name << std::endl;
@@ -62,6 +70,11 @@ bool shm_proxy_pdu_reader_sync(ShmProxyPduType& shm_proxy_pdu)
             auto it = hakoniwa_shm_proxy_pdu->pdu_reader_map.find(reader.name.c_str());
             if (it != hakoniwa_shm_proxy_pdu->pdu_reader_map.end()) {
                 PduReader& pdu_reader = it->second;
+                pdu_reader.buffer.cycle_count++;
+                if (pdu_reader.buffer.cycle_count < pdu_reader.buffer.cycle) {
+                    continue;
+                }
+                pdu_reader.buffer.cycle_count = 0;
                 //std::cout << "INFO: read pdu: robot = " << robot.name << " channel_id = " << reader.channel_id << " pdu_size = " << reader.pdu_size << std::endl;
                 int ret = hako_asset_pdu_read(robot.name.c_str(), reader.channel_id, (char*)(pdu_reader.buffer.data.get()), reader.pdu_size);
                 if (ret != 0) {
@@ -95,6 +108,11 @@ bool shm_proxy_pdu_writer_sync(ShmProxyPduType& shm_proxy_pdu)
             auto it = hakoniwa_shm_proxy_pdu->pdu_writer_map.find(writer.name.c_str());
             if (it != hakoniwa_shm_proxy_pdu->pdu_writer_map.end()) {
                 PduWriter& pdu_writer = it->second;
+                pdu_writer.buffer.cycle_count++;
+                if (pdu_writer.buffer.cycle_count < pdu_writer.buffer.cycle) {
+                    continue;
+                }
+                pdu_writer.buffer.cycle_count = 0;
                 std::lock_guard<std::mutex> lock(hakoniwa_shm_proxy_pdu->mutex);
                 int ret = hako_asset_pdu_write(robot.name.c_str(), writer.channel_id, (const char*)(pdu_writer.buffer.data.get()), writer.pdu_size);
                 if (ret != 0) {
--- virtual/shm-proxy/src/shm_proxy_pdu.hpp
+++ virtual/shm-proxy/src/shm_proxy_pdu.hpp
@@ -11,17 +11,19 @@ struct PduBuffer {
     std::unique_ptr<char[]> data;
     size_t length;
     int lchannel;
-    PduBuffer(int ch, size_t len) : data(new char[len]), length(len), lchannel(ch) {}
+    int cycle;
+    int cycle_count;
+    PduBuffer(int ch, size_t len, int write_cycle) : data(new char[len]), length(len), lchannel(ch), cycle(write_cycle), cycle_count(0) {}
 };
 struct PduReader {
     z_owned_publisher_t pub;
     PduBuffer buffer;
-    PduReader(int ch, size_t len) : buffer(ch, len) {}
+    PduReader(int ch, size_t len, int write_cycle) : buffer(ch, len, write_cycle) {}
 };
 struct PduWriter {
     z_owned_subscriber_t sub;
     PduBuffer buffer;
-    PduWriter(int ch, size_t len) : buffer(ch, len) {}
+    PduWriter(int ch, size_t len, int write_cycle) : buffer(ch, len, write_cycle) {}
 };
 
 typedef struct ShmProxyPdu {
